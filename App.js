e7f17424f7e9aaddf70ea4245cfa85d4a87f4ed0
Santiago Yanguas <santitoya-2001@outlook.com>
2021-11-21 17:00:52 -0500

    [Feature] Add component savings

--- App.js
+++ App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
+import {SafeAreaView, ScrollView} from 'react-native';
 import Header from './components/Header';
 import LaikaMember from './components/LaikaMember';
 import CardBenefitials from './components/CardBenefitials';
+import Savings from './components/Savings';
 
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       {/* <StatusBar /> */}
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={{backgroundColor: 'yellow', height: 10000000}}>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
         <Header />
         <LaikaMember />
         <CardBenefitials />
+        <Savings />
       </ScrollView>
     </SafeAreaView>
   );
